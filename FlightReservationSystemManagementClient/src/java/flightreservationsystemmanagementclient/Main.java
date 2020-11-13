18cc83df966abee8a62f4f5a17f11dc77090e759
Kiyong <angkiyong@gmail.com>
2020-11-13 15:07:10 +0800

    Changes

--- FlightReservationSystemManagementClient/src/java/flightreservationsystemmanagementclient/Main.java
+++ FlightReservationSystemManagementClient/src/java/flightreservationsystemmanagementclient/Main.java
@@ -1,46 +1,46 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package flightreservationsystemmanagementclient;
-//
-//import ejb.session.stateless.AircraftConfigurationSessionBeanRemote;
-//import ejb.session.stateless.EmployeeEntitySessionBeanRemote;
-//import ejb.session.stateless.FareEntitySessionBeanRemote;
-//import ejb.session.stateless.FlightRouteSessionBeanRemote;
-//import ejb.session.stateless.FlightSchedulePlanSessionBeanRemote;
-//import ejb.session.stateless.FlightScheduleSessionBeanRemote;
-//import ejb.session.stateless.FlightSessionBeanRemote;
-//import javax.ejb.EJB;
-//
-///**
-// *
-// * @author Li Xin
-// */
-//public class Main {
-//
-//    @EJB
-//    private static EmployeeEntitySessionBeanRemote employeeEntitySessionBeanRemote;
-//    @EJB
-//    private static AircraftConfigurationSessionBeanRemote aircraftConfigurationSessionBeanRemote;
-//    @EJB
-//    private static FlightRouteSessionBeanRemote flightRouteSessionBeanRemote;
-//    @EJB
-//    private static FlightSessionBeanRemote flightSessionBeanRemote;
-//    @EJB
-//    private static FlightSchedulePlanSessionBeanRemote flightSchedulePlanSessionBeanRemote;
-//    @EJB
-//    private static FareEntitySessionBeanRemote fareEntitySessionBeanRemote;
-//    @EJB
-//    private static FlightScheduleSessionBeanRemote flightScheduleSessionBeanRemote;
-//
-//    /**
-//     * @param args the command line arguments
-//     */
-//    public static void main(String[] args) {
-//        MainApp mainApp = new MainApp(employeeEntitySessionBeanRemote, aircraftConfigurationSessionBeanRemote, flightRouteSessionBeanRemote, flightSessionBeanRemote, flightSchedulePlanSessionBeanRemote, fareEntitySessionBeanRemote, flightScheduleSessionBeanRemote);
-//        mainApp.runApp();
-//    }
-//
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightreservationsystemmanagementclient;
+
+import ejb.session.stateless.AircraftConfigurationSessionBeanRemote;
+import ejb.session.stateless.EmployeeEntitySessionBeanRemote;
+import ejb.session.stateless.FareEntitySessionBeanRemote;
+import ejb.session.stateless.FlightRouteSessionBeanRemote;
+import ejb.session.stateless.FlightSchedulePlanSessionBeanRemote;
+import ejb.session.stateless.FlightScheduleSessionBeanRemote;
+import ejb.session.stateless.FlightSessionBeanRemote;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Li Xin
+ */
+public class Main {
+
+    @EJB
+    private static EmployeeEntitySessionBeanRemote employeeEntitySessionBeanRemote;
+    @EJB
+    private static AircraftConfigurationSessionBeanRemote aircraftConfigurationSessionBeanRemote;
+    @EJB
+    private static FlightRouteSessionBeanRemote flightRouteSessionBeanRemote;
+    @EJB
+    private static FlightSessionBeanRemote flightSessionBeanRemote;
+    @EJB
+    private static FlightSchedulePlanSessionBeanRemote flightSchedulePlanSessionBeanRemote;
+    @EJB
+    private static FareEntitySessionBeanRemote fareEntitySessionBeanRemote;
+    @EJB
+    private static FlightScheduleSessionBeanRemote flightScheduleSessionBeanRemote;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainApp mainApp = new MainApp(employeeEntitySessionBeanRemote, aircraftConfigurationSessionBeanRemote, flightRouteSessionBeanRemote, flightSessionBeanRemote, flightSchedulePlanSessionBeanRemote, fareEntitySessionBeanRemote, flightScheduleSessionBeanRemote);
+        mainApp.runApp();
+    }
+
+}
