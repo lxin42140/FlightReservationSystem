3542dfb1eb9cbddba97144e76581b09549496bfd
lxin42140 <lxin42140@gmail.com>
2020-11-15 19:29:26 +0800

    Fixes

--- FlightReservationSystemLibrary/src/ejb/session/stateless/FlightReservationSessionBeanRemote.java
+++ FlightReservationSystemLibrary/src/ejb/session/stateless/FlightReservationSessionBeanRemote.java
@@ -22,10 +22,8 @@ import util.exception.CreateNewFlightReservationException;
 public interface FlightReservationSessionBeanRemote {
 
     public FlightReservationEntity viewFlightReservationByFlightReservationId(Long flightReservationId);
-<<<<<<< HEAD
     
     public List<FlightReservationEntity> viewFlightReservationByUser(Long customerId);
-
 
     public Long createNewFlightReservation(List<FlightScheduleEntity> itinery, List<PassengerEntity> passengers, CreditCardEntity creditCardEntity, UserEntity user) throws CreateNewFlightReservationException;
 }
